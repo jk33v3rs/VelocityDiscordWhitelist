1d92cbf9dae797361f472005a560b35705d150d5
Rathinosk <4714624+rathinosk@users.noreply.github.com>
2025-03-19 01:58:30 -0400

    feat: Add shadowJar task to CI workflow and include copyright notices in source files

--- src/main/java/top/rathinosk/velocitywhitelist/VelocityWhitelist.java
+++ src/main/java/top/rathinosk/velocitywhitelist/VelocityWhitelist.java
@@ -1,3 +1,11 @@
+// This file is part of VelocityWhitelist.
+// Copyright (C) 2025 Rathinosk
+//
+// VelocityWhitelist is free software: you can redistribute it and/or modify
+// it under the terms of the MIT License. VelocityWhitelist is distributed in
+// the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+// the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
 package top.rathinosk.velocitywhitelist;
 
 import com.google.inject.Inject;
